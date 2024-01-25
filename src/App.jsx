ca1996ac7c6b248718c1741aa86db4649e54e766
Sheraz Arif <sherazarifofficial@gmail.com>
2024-01-25 23:24:13 +0500

    added another section name spirtual

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Section1 from "./Components/Section1/Section1";
 import CardSection from "./Components/CardSection/CardSection";
 import CoursesSection from "./Components/CoursesSection/CoursesSection";
+import SpirtualSection from "./Components/SpirtualSection/SpirtualSection";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Section1 />
       <CardSection />
       <CoursesSection />
+      <SpirtualSection />
     </div>
   );
 }
---
+++ src/Components/SpirtualSection/SpirtualSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SpirtualComp from "./SpirtualComp";
+
+const SpirtualSection = () => {
+  return (
+    <div>
+      <SpirtualComp />
+    </div>
+  );
+};
+
+export default SpirtualSection;
